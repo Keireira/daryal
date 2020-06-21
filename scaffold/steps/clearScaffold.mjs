c336341db2b6532e3907925902b2c0d9b04b8c9f
Alena Dzhukich <github@alena.red>
2020-06-22 01:26:16 +0300

    Add more cleaning steps in the clearScaffold

--- scaffold/steps/clearScaffold.mjs
+++ scaffold/steps/clearScaffold.mjs
@@ -2,17 +2,31 @@ import fs from 'fs'
 import path from 'path'
 
 const clearScaffold = async () => {
-	const promise = new Promise((resolve, reject) => {
+	const scaffoldPromise = new Promise((resolve, reject) => {
 		fs.rmdir(path.resolve('./scaffold'), { recursive: true }, (err) => {
-			if (err) {
-				reject(err)
-			}
+			if (err) reject(err)
 
 			resolve()
 		})
 	})
 
-	return promise
+	const nodeModulesPromise = new Promise((resolve, reject) => {
+		fs.rmdir(path.resolve('./node_modules'), { recursive: true }, (err) => {
+			if (err) reject(err)
+
+			resolve()
+		})
+	})
+
+	const yarnPromise = new Promise((resolve, reject) => {
+		fs.unlink(path.resolve('./yarn.lock'), (err) => {
+			if (err) reject(err)
+
+			resolve()
+		})
+	})
+
+	return Promise.all([scaffoldPromise, nodeModulesPromise, yarnPromise])
 }
 
 export default clearScaffold
